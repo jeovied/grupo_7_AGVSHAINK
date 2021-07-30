fe09c848215bdb809af5bfa074c8d004c7d0e42f
CamilaAguirre43 <camilaaguirre458@gmail.com>
2021-07-30 00:20:19 -0300

    agrega las vistas productAdd, productEdit

--- site/src/controllers/productsController.js
+++ site/src/controllers/productsController.js
@@ -6,5 +6,11 @@ module.exports = {
     },
     cart : (req,res) => {
         return res.render(path.join(__dirname, '..', 'views', 'products', 'productCart'));
+    },
+    add : (req,res) => {
+        return res.render(path.join(__dirname,'..','views','products','productAdd'));
+    },
+    edit : (req,res) => {
+        return res.render(path.join(__dirname,'..','views','products','productEdit'));
     }
 }
--- site/src/routes/products.js
+++ site/src/routes/products.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const {detail, cart} = require('../controllers/productsController');
+const {detail, cart,add,edit} = require('../controllers/productsController');
 
 router.get('/detail', detail);
 router.get('/cart', cart);
+router.get('/add', add);
+router.get('/edit', edit);
 
 module.exports = router;
